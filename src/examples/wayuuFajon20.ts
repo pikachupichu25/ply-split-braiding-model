c7eb852ec4f618200f64632a1ee42bbdc0c7a963
kp <korrawee.p@protonmail.com>
2026-09-07 16:15:44 +0700

    Update eyes pattern

--- src/examples/wayuuFajon20.ts
+++ src/examples/wayuuFajon20.ts
@@ -1,5 +1,6 @@
 export const wayuuFajon20Pattern = `# Eyes
-color: AABCBBCBAAAABCBBCBAA
+# color: AABCBBCBAAAABCBBCBAA
+color:   ABCBAABCBAABCBAABCBA
 palette: A=brown, B=white, C=lightblue
 
 1 15>16,17,18,19,20
@@ -7,7 +8,7 @@ palette: A=brown, B=white, C=lightblue
 3 5>6,7,8,9,10,11
 4 5>4,3,2,1
 
-[repeat 1-4 x 4]
+[repeat 1-4 x 5]
 
 5 5>6
 6 15>16
@@ -19,10 +20,4 @@ palette: A=brown, B=white, C=lightblue
 10 1>2,3,4,5,6
 [repeat 7-10 x 5]
 
-11 10>11
-
-12 15>16,17,18,19,20
-13 15>14,13,12,11
-14 5>6,7,8,9,10,11
-15 5>4,3,2,1
-[repeat 12-15 x 4]`;
+11 10>11`;
